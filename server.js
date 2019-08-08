32a3b743a752b1acaeb15fa642f29599a2a635f4
Gabriel <jgj_junior@yahoo.com.br>
2019-08-07 21:23:44 -0300

    2

--- server.js
+++ server.js
@@ -1,11 +1,44 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+const database = {
+	users: [
+		{
+			id: 1,
+			name: "NAME",
+			email: "EMAIL",
+			password: "PASSWORD",
+			entries: 0,
+			joined: new Date()
+		},
+		{
+			id: 2,
+			name: "NAME2",
+			email: "EMAIL2",
+			password: "PASSWORD2",
+			entries: 0,
+			joined: new Date()
+		}
+	]
+};
+
 app.listen(3000, () => {
-  console.log("hello")
-})
-app.get("", (req, res) => {
-  res.send("working")
-})
+	console.log("listen");
+});
+app.get("", (req, resp) => {
+	resp.send("hello");
+});
+app.post("/signin", (req, resp) => {
+	if (
+		req.body.email === database.users[0].email &&
+		req.body.password === database.users[0].password
+	) {
+		resp.json("sucess");
+	} else {
+		resp.status(400).json("error");
+	}
+});
 
 /*
 res = working
